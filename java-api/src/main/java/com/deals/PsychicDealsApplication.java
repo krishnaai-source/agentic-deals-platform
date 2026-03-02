b82c4b66269195650bcafa561c3b16b3b9806003
krishnaai-source <ramakrishna.padavala@gmail.com>
2026-03-02 08:20:04 -0600

    Create PsychicDealsApplication.java

---
+++ java-api/src/main/java/com/deals/PsychicDealsApplication.java
@@ -0,0 +1,11 @@
+package com.deals.psychicdeals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsychicDealsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PsychicDealsApplication.class, args);
+    }
+}
