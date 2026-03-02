cccfea8bd1c8ff78e9bd2043eda4fe8e255885a4
krishnaai-source <ramakrishna.padavala@gmail.com>
2026-03-01 20:47:48 -0600

    Create DealRequest.java

---
+++ java-api/src/com/bestbuy/psychicdeals/dto/DealRequest.java
@@ -0,0 +1,14 @@
+// DealRequest.java
+package com.bestbuy.psychicdeals.dto;
+
+import java.util.List;
+
+public class DealRequest {
+    private String customerId;
+    private String region;
+    private String intentQuery;
+    private String velocityType;
+    private List<String> preferredCategories;
+
+    // getters and setters
+}
